25990057a8a8908d88177a3aa77a2b2a6d827c74
ChrisGenti <gentile.christian@outlook.it>
2025-01-21 12:35:38 +0100

    Fix ILuckPermsPlayer.java

--- src/main/java/net/silexmc/minestom/luckperms/api/ILuckPermsPlayer.java
+++ src/main/java/net/silexmc/minestom/luckperms/api/ILuckPermsPlayer.java
@@ -1,19 +1,11 @@
 package net.silexmc.minestom.luckperms.api;
 
 import net.kyori.adventure.text.Component;
-import net.minestom.server.entity.Player;
-import net.minestom.server.network.player.GameProfile;
-import net.minestom.server.network.player.PlayerConnection;
-import org.jetbrains.annotations.NotNull;
 
-public abstract class ILuckPermsPlayer extends Player {
-    public ILuckPermsPlayer(@NotNull PlayerConnection connection, @NotNull GameProfile profile) {
-        super(connection, profile);
-    }
+public interface ILuckPermsPlayer {
+    boolean hasPermission(String permission);
 
-    public abstract boolean hasPermission(String permission);
+    Component getPrefix();
 
-    public abstract Component getPrefix();
-
-    public abstract Component getSuffix();
+    Component getSuffix();
 }
